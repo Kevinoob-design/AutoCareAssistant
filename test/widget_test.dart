cfb28c42accf48e938183c7c5d1bedfdbba85e1b
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2025-03-21 19:26:32 -0400

    feat: change test files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:auto_care_assistant/app.dart' show MyApp;
+import 'package:auto_care_assistant/splash/splash_controller.dart';
+import 'package:auto_care_assistant/splash/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp(settingsController: null));
+    await tester.pumpWidget(SplashScreen(controller: SplashController()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
