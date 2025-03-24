8497d8f24d27a2c59b650c164ba84e6089943f63
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2025-03-24 15:12:50 -0400

    feat: added firebase snapshot to persist signin

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,15 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
 
   static const routeName = '/home';
+
+  final fireBaseAuth = FirebaseAuth.instanceFor(app: Firebase.app());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(actions: [Icon(Icons.notifications)]),
-      body: Text('Welcome Home!'),
+      body: Text(fireBaseAuth.currentUser?.uid as String),
     );
   }
 }
